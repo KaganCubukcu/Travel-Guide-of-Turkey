d93585a979f637ef39624ab65dd6b67ab57187ba
Kağan Çubukçu <kagancancubukcu@gmail.com>
2022-10-13 10:25:42 +0300

    Landing page componenti oluşturuldu

---
+++ src/components/landing_page/Landing.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../navbar/Navbar";
+
+const Landing = () => {
+  return (
+    <div className="bg-landing bg-cover w-screen h-screen">
+      <Navbar />
+    </div>
+  );
+};
+
+export default Landing;
