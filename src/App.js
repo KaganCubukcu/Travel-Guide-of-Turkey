611b91fcaa04d9953abb293c64ce6918c7828b5c
Kağan Çubukçu <kagancancubukcu@gmail.com>
2022-10-18 12:21:41 +0300

    Main componenti eklendi

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Landing from "./components/landing_page/Landing";
+import Main from "./components/main/Main";
 function App() {
   return (
     <div>
       <Landing />
+      <Main />
     </div>
   );
 }
