81a2ead8c29d6a46791905945e8e7393d4d6ca9b
Kağan Çubukçu <kagancancubukcu@gmail.com>
2022-10-18 12:23:24 +0300

    Landing page giriş kısmı tamamlandı

--- src/components/landing_page/Landing.jsx
+++ src/components/landing_page/Landing.jsx
@@ -5,6 +5,12 @@ const Landing = () => {
   return (
     <div className="bg-landing bg-cover w-screen h-screen">
       <Navbar />
+      <div className="flex justify-center pt-[215px] flex-col items-center">
+        <h1 className="text-white text-9xl pt-[115px] font-bold">TÜRKİYE</h1>
+        <h2 className="text-white text-6xl pt-5">
+          The <span className="italic">new</span> dawn of journey
+        </h2>
+      </div>
     </div>
   );
 };
