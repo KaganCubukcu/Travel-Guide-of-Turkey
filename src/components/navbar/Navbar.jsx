1cd9015c0f942fb14d68d265d46473e879175e06
Kağan Çubukçu <kagancancubukcu@gmail.com>
2022-10-18 12:20:49 +0300

    Navbar'ın eksikleri tamamlandı iconlar düzeltildi

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import turkeyLogo from "../../assets/turkiye-logo 1.svg";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 const Navbar = () => {
   return (
-    <div className="container mx-auto bg-red-500 h-16 text-white flex justify-between items-center">
-      <div className="mt-2">
+    <div className="container mx-auto h-16 pt-5 text-white flex justify-between items-center ">
+      <div className="cursor-pointer">
         <img src={turkeyLogo} alt="turkey-logo" />
       </div>
-      <div className="flex">
-        <div>About us</div>
-        <div>Contacts</div>
-        <div></div>
-        <div></div>
+      <div className="flex gap-10 h-[76px] items-center content-end text-2xl">
+        <div className="cursor-pointer">About us</div>
+        <div className="cursor-pointer">Contacts</div>
+        <FontAwesomeIcon
+          icon={faMagnifyingGlass}
+          className="w-6 h-6 cursor-pointer"
+        />
+        <FontAwesomeIcon icon={faBars} className="w-6 h-6 cursor-pointer" />
       </div>
     </div>
   );
