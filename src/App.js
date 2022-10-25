c548156ca7c589470c7bbaba88e358047c4db159
Kağan Çubukçu <kagancancubukcu@gmail.com>
2022-10-25 11:19:47 +0300

    experience section eklendi

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Landing from "./components/landing_page/Landing";
-import Main from "./components/main/Main";
+import Main from "./components/main_cities/Main";
+import Experience from "./components/main_experiences/Experience";
 function App() {
   return (
     <div>
       <Landing />
       <Main />
+      <Experience />
     </div>
   );
 }
