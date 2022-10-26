2c0464fc69e34b8a0649387a89b53defbdebe1f5
Kağan Çubukçu <kagancancubukcu@gmail.com>
2022-10-26 09:50:29 +0300

    Region, Gastro ve Footer componentleri eklendi

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import Footer from "./components/footer/Footer";
 import Landing from "./components/landing_page/Landing";
 import Main from "./components/main_cities/Main";
 import Experience from "./components/main_experiences/Experience";
+import Gastro from "./components/main_gastro/Gastro";
+import Region from "./components/main_region/Region";
+
 function App() {
   return (
     <div>
       <Landing />
       <Main />
       <Experience />
+      <Region />
+      <Gastro />
+      <Footer />
     </div>
   );
 }
