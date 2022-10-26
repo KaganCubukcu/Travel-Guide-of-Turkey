22fc54646e919e351fcf0dc60c2a63714cefc1e9
Kağan Çubukçu <kagancancubukcu@gmail.com>
2022-10-26 09:51:40 +0300

    Region componenti eklendi

---
+++ src/components/main_region/Card.jsx
@@ -0,0 +1,36 @@
+import regions from "./regions.json";
+
+const Card = () => {
+  return (
+    <div className="my-[45px]">
+      <div className="gap-3 grid grid-cols-4">
+        {regions.slice(0, 4).map((region) => (
+          <div
+            key={region.id}
+            className="w-[350px] h-[233px] mr-10 mb-5 bg-cover bg-center rounded-3xl cursor-pointer flex items-center"
+            style={{ backgroundImage: `url(${region.background})` }}
+          >
+            <p className="mb-5 transform w-[239px] h-[239px] -rotate-90 text-white font-bold text-3xl">
+              {region.name}
+            </p>
+          </div>
+        ))}
+      </div>
+      <div className="flex items-center justify-evenly">
+        {regions.slice(-3).map((region) => (
+          <div
+            key={region.id}
+            className="w-[350px] h-[233px] mr-10 my-5 bg-cover bg-center rounded-3xl cursor-pointer flex items-center"
+            style={{ backgroundImage: `url(${region.background})` }}
+          >
+            <p className="mb-5 transform w-[239px] h-[239px] -rotate-90 text-white font-bold text-3xl">
+              {region.name}
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Card;
