07c140d038ca927616d977e905a96cc02113ce30
Kağan Çubukçu <kagancancubukcu@gmail.com>
2022-10-26 09:51:47 +0300

    Footer eklendi

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,20 @@
+const Footer = () => {
+  return (
+    <div className="bg-black">
+      {/* Rengi Değişecek */}
+      <div className="pt-[116px] container mx-auto ">
+        <h3 className="text-6xl	text-white">
+          A wonderland <br />
+          of tastes
+        </h3>
+        <p className="text-white pt-[45px] max-w-[852px]">
+          Türkiye is home to the freshest and tastiest fruits and vegetables
+          since it enjoys a temperate climate with all four seasons. The food
+          here is mind-blowing! It is a true culinary paradise!
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
