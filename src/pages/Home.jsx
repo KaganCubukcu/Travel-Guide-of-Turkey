8d0159c186d13f167eff4de0b2bbdebe21c7211b
Kağan Çubukçu <kagancancubukcu@gmail.com>
2022-10-27 08:38:09 +0300

    Landing page tek bir componentte toplandı

---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import Landing from "../components/landing_page/Landing";
+import Main from "../components/main_cities/Main";
+import Experience from "../components/main_experiences/Experience";
+import Gastro from "../components/main_gastro/Gastro";
+import Region from "../components/main_region/Region";
+const Home = () => {
+  return (
+    <>
+      <Landing />
+      <Main />
+      <Experience />
+      <Region />
+      <Gastro />
+    </>
+  );
+};
+
+export default Home;
