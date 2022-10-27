780498e9f9c312001f78ebfd7af4ac59df753491
Kağan Çubukçu <kagancancubukcu@gmail.com>
2022-10-27 08:36:18 +0300

    Routes eklendi

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import Footer from "./components/footer/Footer";
-import Landing from "./components/landing_page/Landing";
-import Main from "./components/main_cities/Main";
-import Experience from "./components/main_experiences/Experience";
-import Gastro from "./components/main_gastro/Gastro";
-import Region from "./components/main_region/Region";
+import Navbar from "./components/navbar/Navbar";
+import Home from "./pages/Home";
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Landing />
-      <Main />
-      <Experience />
-      <Region />
-      <Gastro />
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
       <Footer />
-    </div>
+    </>
   );
 }
 
