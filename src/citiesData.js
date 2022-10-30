41d2b074e8a2760295052fac8801f24c6cf5745e
Kağan Çubukçu <kagancancubukcu@gmail.com>
2022-10-30 09:34:25 +0300

    Şehir dataları eklendi -İzmir, Erzurum

--- src/citiesData.js
+++ src/citiesData.js
@@ -1,28 +1,44 @@
 export const cities = [
   {
     name: "İzmir",
-    p: "",
-    catogories: [
-      {
-        see: [
-          {
-            bg: "",
-            p: "",
-          },
-        ],
-        touch: [
-          {
-            bg: "",
-            p: "",
-          },
-        ],
-        taste: [
-          {
-            bg: "",
-            p: "",
-          },
-        ],
+    id: 1,
+    p: "'Beautiful İzmir' draws you into a world of picturesque modernity and traditions, with its refreshing touches to human souls. İzmir hypnotizes visitors in an aura of history and modernity.",
+    catogories: {
+      see: {
+        bg: "https://3.bp.blogspot.com/-ZOi6tYtP9XA/TgCub43GxHI/AAAAAAAAAvs/J9oidiGCFBM/s1600/bayrakl%25C4%25B1-smyrna.jpg",
+        p: "Ancient Smyrna is located on the mound in İzmir Bayraklı. It is a small peninsula that was surrounded by the sea to the west and south in ancient times. ",
       },
-    ],
+
+      touch: {
+        bg: "https://www.bizevdeyokuz.com/wp-content/uploads/kizlaragasi-hani-kemeralti-izmir.jpg",
+        p: "In the Kemaraltı Historical Bazaar you will find products from the skilled hands of İzmir that bring together the traces of the past with the texture of the present. ",
+      },
+
+      taste: {
+        bg: "https://www.ardaninmutfagi.com/wp-content/uploads/2012/08/izmir-k%C3%B6fte.jpg",
+        p: "A regional favourite, meatballs cooked in tomatoe sauce, green peppers and potatoes takes the city’s name as İzmir Köfte (meatballs). ",
+      },
+    },
+  },
+  {
+    name: "Erzurum",
+    id: 1,
+    p: "Going back millenia, Erzurum is one of the oldest cities of Anatolia, and the center of cultural and natural richness of the Eastern Anatolian Region.",
+    catogories: {
+      see: {
+        bg: "https://pbs.twimg.com/media/D6MCDW3U0AAXLkM?format=jpg&name=4096x4096",
+        p: "Erzurum is famous for its historical artifacts that remain intact for thousands of years, natural beauties and a legendary cuisine.",
+      },
+
+      touch: {
+        bg: "https://kulturveyasam.com/wp-content/uploads/2017/12/oltu-tasi-03.jpg",
+        p: "Erzurum is world famous for Oltu stone products. A fossilized juniper product, jet stone is incorporated into countless varieties of souvenir and jewelry. ",
+      },
+
+      taste: {
+        bg: "https://gelgorcagkebap.com.tr/wp-content/uploads/2016/05/headerSlide1.jpg",
+        p: "Bearing the traces of the past, Erzurum cuisine is calling you with its unique tastes. Make sure you taste the dishes prepared with organic milk and meat products of the animals fed at an altitude of 1800 to 3200 meters. ",
+      },
+    },
   },
 ];
