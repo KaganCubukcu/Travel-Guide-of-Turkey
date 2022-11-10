7829dda869dfd9b46d1173ad02f5c6e328d305d3
Kağan Çubukçu <kagancancubukcu@gmail.com>
2022-11-10 22:30:40 +0300

    style düzeltildi

--- src/components/cities_info/Cities.jsx
+++ src/components/cities_info/Cities.jsx
@@ -12,12 +12,12 @@ const CitiesSee = () => {
           <>
             <div
               key={city.id}
-              className="bg-cover bg-center	 h-screen text-white sticky top-0"
+              className="bg-cover bg-center text-white sticky top-0"
               style={{
                 backgroundImage: `url(${city.catogories.see.bg})`,
               }}
             >
-              <div className="w-[654px] h-[1080px] flex flex-col justify-center pl-[120px]">
+              <div className="w-[654px] h-[1080px] flex flex-col justify-center pl-[120px] bg-gradient-to-r from-[#252525]">
                 <h1 className="text-8xl">See</h1>
                 <p className="mt-[30px] mb-[60px]">{city.catogories.see.p}</p>
                 <button className="bg-main-bg flex justify-center items-center rounded-xl w-[100px] h-[40px] hover:bg-[#2a2a2a] ease-in-out duration-300 ">
@@ -27,12 +27,12 @@ const CitiesSee = () => {
             </div>
             {/* Touch */}
             <div
-              className=" bg-cover h-screen text-white flex justify-end sticky top-0"
+              className=" bg-cover bg-center text-white flex justify-end sticky top-0"
               style={{
                 backgroundImage: `url(${city.catogories.touch.bg})`,
               }}
             >
-              <div className="w-[654px] flex flex-col justify-center pr-[120px]">
+              <div className="w-[654px] h-[1080px] flex flex-col justify-center pr-[120px] bg-gradient-to-l from-[#252525]">
                 <h1 className="text-8xl">Touch</h1>
                 <p className="mt-[30px] mb-[60px]">{city.catogories.touch.p}</p>
                 <button className="bg-main-bg flex justify-center items-center rounded-xl w-[100px] h-[40px] hover:bg-[#2a2a2a] ease-in-out duration-300 ">
@@ -42,12 +42,12 @@ const CitiesSee = () => {
             </div>
             {/* Taste */}
             <div
-              className="bg-landing bg-cover h-screen text-white sticky top-0 "
+              className="bg-cover bg-center text-white sticky top-0 "
               style={{
                 backgroundImage: `url(${city.catogories.taste.bg})`,
               }}
             >
-              <div className="w-[654px] h-[1080px] flex flex-col justify-center pl-[120px]">
+              <div className="w-[654px] h-[1080px] flex flex-col justify-center pl-[120px] bg-gradient-to-r from-[#252525]">
                 <h1 className="text-8xl">Taste</h1>
                 <p className="mt-[30px] mb-[60px]">{city.catogories.taste.p}</p>
                 <button className="bg-main-bg flex justify-center items-center rounded-xl w-[100px] h-[40px] hover:bg-[#2a2a2a] ease-in-out duration-300 ">
