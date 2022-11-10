f61266c46e60d95762e2b9b4e37ca670f22e9b07
Kağan Çubukçu <kagancancubukcu@gmail.com>
2022-11-10 22:29:11 +0300

    Experience route eklendi

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import Home from "./pages/Home";
 import { Routes, Route } from "react-router-dom";
 import CitiesInfo from "./pages/CitiesInfo";
-
+import Experience from "./pages/Experience";
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cities/:cityname" element={<CitiesInfo />} />
+        <Route path="/experience/:experienceName" element={<Experience />} />
       </Routes>
     </>
   );
