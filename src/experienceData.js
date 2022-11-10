371ca2384bcee04a567e31c3476fdfd3aa4925f4
Kağan Çubukçu <kagancancubukcu@gmail.com>
2022-11-10 22:30:15 +0300

    ExperienceData oluşturuldu

---
+++ src/experienceData.js
@@ -0,0 +1,102 @@
+export const experiences = [
+  {
+    URL: "Wellness",
+    name: "Wellness",
+    id: 1,
+    p: "Traditional Spa Wellness Therapies are one of the best ways to overcome the barriers preventing us from being the best versions of ourselves.",
+    slides: [
+      " https://i0.wp.com/istanbulvacationtips.com/wp-content/uploads/2019/10/what-to-expect-turkish-hamam-istanbul-vacation-tips.jpg",
+      " https://www.ramadaplazaistanbul.com/images/thumb/1920x1080/gal_02082019_ramadaplazaistanbulcenterbeatusspa_3.jpg",
+      "https://www.sivastermal.com.tr/img/spa-hamam/spa-1.jpg",
+    ],
+    categories: [
+      // section1
+      {
+        name: "Wellness",
+        bg: "https://sevamuhendislik.com.tr/admin/dimg/projeler/62028fa180923.jpg",
+        p: "The word “Spa” implies health through water, of feeling rejuvenated by relaxing in hot mineral waters.",
+      },
+      // section2
+      {
+        name: "Thermals & Springs",
+        bg: "https://www.pointholiday.com/ResimGoster.aspx?resim=Agac/58/PAMUKKALE_1_.JPG",
+        p: "Natural Hot Springs have been resources to prevent and treat diseases, reduce stress and improve the quality of life since antiquity.",
+      },
+      // section3
+      {
+        name: "Turkish Hamam",
+        bg: "https://www.proartsauna.com/files/2020/11/klasik-turk-hamami-28.jpg",
+        p: "The tradition of purification through cleansing is a therapeutic ritual that extends back to Ancient Rome and is an essential part of Turkish culture.",
+      },
+    ],
+  },
+  {
+    URL: "Outdoor&adventure",
+    name: "Outdoor & Adventure",
+    id: 2,
+    p: "Outdoor activities and adventure opportunities are endless in Türkiye. Breathtaking natural beauty awaits to be discovered by outdoor enthusiasts.",
+    slides: [
+      // image 1
+      "https://tatiliyidir.com/wp-content/uploads/2020/08/fethiye-parasut-tatiliyidir2.jpg",
+      // image 2
+      "https://cdnuploads.aa.com.tr/uploads/VideoGallery/2021/02/21/adffff77c74e10c2d164f2a158acdbf2.jpg",
+      //image 3
+      "https://www.essencetourismtravel.com/wp-content/uploads/2017/05/www.essenceholiday.com_-5.jpg",
+    ],
+    categories: [
+      {
+        name: "Trekking & Hiking",
+        bg: "https://cdn.goturkiye.com/sports/trekking-hiking-desktop-ana-sayfa-kapak.jpg",
+        p: "Trekking allows you to get away from the crowds and discover the true beauty of Türkiye, the friendly villagers, the remote historic sites, and the country’s stunning scenery.",
+      },
+
+      {
+        name: "Rock climbing",
+        bg: "https://www.discovergreece.com/sites/default/files/2020-12/You%20can%20be%20inverted%20on%20an%20overhang%20with%20the%20incredible%20colours%20of%20the%20Aegean%20as%20a%20backdrop%20in%20the%20morning.jpg",
+        p: "Türkiye is a truly unmatched destination for rock climbers with crags for every season, a variety of available rock-climbing styles, and friendly, hospitable locals.",
+      },
+
+      {
+        name: "Skydiving",
+        bg: "https://outdoorturkiye.com.tr/storage/blog/c561622c1bb47f69bd6d0bbc3eaf1156.jpg",
+        p: "Skydiving is an incredibly easy, safe, and enjoyable way to experience an unforgettable journey through Türkiye’s blue sky!",
+      },
+      {
+        name: "Rock climbing",
+        bg: "https://www.discovergreece.com/sites/default/files/2020-12/You%20can%20be%20inverted%20on%20an%20overhang%20with%20the%20incredible%20colours%20of%20the%20Aegean%20as%20a%20backdrop%20in%20the%20morning.jpg",
+        p: "Türkiye is a truly unmatched destination for rock climbers with crags for every season, a variety of available rock-climbing styles, and friendly, hospitable locals.",
+      },
+    ],
+  },
+  {
+    URL: "Learn&study",
+    name: "Learn & Study",
+    id: 3,
+    p: "Modern Türkiye places great emphasis on education and young people. It is the only country in the world that has two public holidays with children and youth as central themes.",
+    slides: [
+      // image 1
+      "https://cdn.istanbul.edu.tr/FileHandler.ashx?f=8WQmTovHYUCxR0js0IkW6Q",
+      // image 2
+      "https://cevizgo.com/wp-content/uploads/2022/02/ankara-kutuphaneler-listesi-ankarada-ucretsiz-kutuphaneler-cevizgo-enel-kultur.jpg",
+      //image 3
+      "https://egeajans.ege.edu.tr/wp-content/uploads/2020/07/t%C4%B1p1.jpg",
+    ],
+    categories: [
+      {
+        name: "Learning & studying",
+        bg: "https://uludag.edu.tr/dosyalar/anasayfa/haber_resimler/2020/Haziran/yaz-okulu-online-devam-edecek/2.jpeg",
+        p: "Choosing where you want to go in Türkiye is one of the biggest decisions that you need to make. ",
+      },
+      {
+        name: "Learning Turkish",
+        bg: "https://media.thegospelcoalition.org/wp-content/uploads/2011/02/17012750/janko-ferlic-174927-unsplash-1.jpg",
+        p: "Turkish is major member of Turkic Language Family and today it is spoken by around 100 million people.",
+      },
+      {
+        name: "Youth travelling and volunteering",
+        bg: "https://cdn.goturkiye.com/learning/youth-travelling.jpg",
+        p: "It is never early to start thinking about internship or volunteering opportunities.",
+      },
+    ],
+  },
+];
