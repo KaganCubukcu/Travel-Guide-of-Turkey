d98b0a831add4a9e7ca2e35b9dc0dbf6638966d2
Kağan Çubukçu <kagancancubukcu@gmail.com>
2022-11-21 08:12:56 +0300

    Region comp. silindi

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,6 @@ import Landing from "../components/landing_page/Landing";
 import Main from "../components/main_cities/Main";
 import Experience from "../components/main_experiences/Experience";
 import Gastro from "../components/main_gastro/Gastro";
-import Region from "../components/main_region/Region";
 
 const Home = () => {
   return (
