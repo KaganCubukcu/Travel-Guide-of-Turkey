8b34fcdcb7a37cfedf136eb6009a0ce5dda76c1c
Kağan Çubukçu <kagancancubukcu@gmail.com>
2022-11-21 15:00:42 +0300

    Link eklendi

--- src/components/cities_info/Cities.jsx
+++ src/components/cities_info/Cities.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { cities } from "../../citiesData";
 
 const CitiesSee = () => {
@@ -20,9 +20,11 @@ const CitiesSee = () => {
               <div className="w-[654px] h-[1080px] flex flex-col justify-center pl-[120px] bg-gradient-to-r from-[#252525]">
                 <h1 className="text-8xl">See</h1>
                 <p className="mt-[30px] mb-[60px]">{city.catogories.see.p}</p>
-                <button className="bg-main-bg flex justify-center items-center rounded-xl w-[100px] h-[40px] hover:bg-[#2a2a2a] ease-in-out duration-300 ">
-                  Get more
-                </button>
+                <Link to={`/cities/getmore/see-${city.name}`}>
+                  <button className="bg-main-bg flex justify-center items-center rounded-xl w-[100px] h-[40px] hover:bg-[#2a2a2a] ease-in-out duration-300 ">
+                    Get more
+                  </button>
+                </Link>
               </div>
             </div>
             {/* Touch */}
@@ -35,9 +37,11 @@ const CitiesSee = () => {
               <div className="w-[654px] h-[1080px] flex flex-col justify-center pr-[120px] bg-gradient-to-l from-[#252525]">
                 <h1 className="text-8xl">Touch</h1>
                 <p className="mt-[30px] mb-[60px]">{city.catogories.touch.p}</p>
-                <button className="bg-main-bg flex justify-center items-center rounded-xl w-[100px] h-[40px] hover:bg-[#2a2a2a] ease-in-out duration-300 ">
-                  Get more
-                </button>
+                <Link to={`/cities/getmore/touch-${city.name}`}>
+                  <button className="bg-main-bg flex justify-center items-center rounded-xl w-[100px] h-[40px] hover:bg-[#2a2a2a] ease-in-out duration-300 ">
+                    Get more
+                  </button>
+                </Link>
               </div>
             </div>
             {/* Taste */}
@@ -50,9 +54,11 @@ const CitiesSee = () => {
               <div className="w-[654px] h-[1080px] flex flex-col justify-center pl-[120px] bg-gradient-to-r from-[#252525]">
                 <h1 className="text-8xl">Taste</h1>
                 <p className="mt-[30px] mb-[60px]">{city.catogories.taste.p}</p>
-                <button className="bg-main-bg flex justify-center items-center rounded-xl w-[100px] h-[40px] hover:bg-[#2a2a2a] ease-in-out duration-300 ">
-                  Get more
-                </button>
+                <Link to={`/cities/getmore/taste-${city.name}`}>
+                  <button className="bg-main-bg flex justify-center items-center rounded-xl w-[100px] h-[40px] hover:bg-[#2a2a2a] ease-in-out duration-300 ">
+                    Get more
+                  </button>
+                </Link>
               </div>
             </div>
           </>
