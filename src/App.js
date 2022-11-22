f678d47143d059fb39605cad50b58ab68339879d
Kağan Çubukçu <kagancancubukcu@gmail.com>
2022-11-22 10:38:58 +0300

    Allcities ve Allexperiences componentleri eklendi

--- src/App.js
+++ src/App.js
@@ -5,11 +5,15 @@ import Experience from "./pages/Experience";
 import Footer from "./components/footer/Footer";
 import GetmoreExpPage from "./pages/GetmoreExpPage";
 import GetmoreCityPage from "./pages/GetmoreCityPage";
+import Allcities from "./pages/Allcities";
+import Allexperience from "./pages/Allexperience";
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/All-cities" element={<Allcities />} />
+        <Route path="/All-experiences" element={<Allexperience />} />
         <Route path="/cities/:cityname" element={<CitiesInfo />} />
         <Route path="/experience/:experienceName" element={<Experience />} />
         <Route
